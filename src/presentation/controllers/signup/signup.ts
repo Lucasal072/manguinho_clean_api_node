53f092696c47050af9c660b10a4aa22bb5f01e16
lucas alves <lucas@pop-os.localdomain>
2024-01-28 02:08:13 -0300

    refactor: move success http response to a helper

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { type Controller, type HttpResponse, type HttpRequest, type EmailValidator, type AddAccount } from './sigup-protocols'
 
 export class SignUpController implements Controller {
@@ -31,10 +31,7 @@ export class SignUpController implements Controller {
         email,
         password
       })
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
