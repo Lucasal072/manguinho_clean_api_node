af56bfa9247430cab3aca5a3b9b2e4177b134814
lucas alves <lucas@pop-os.localdomain>
2024-01-27 22:10:45 -0300

    refactor: move a server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,14 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
+import { type EmailValidator } from '../protocols/email-validator'
 import { type HttpResponse, type HttpRequest } from '../protocols/http'
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) { }
+  constructor (private readonly emailValidator: EmailValidator) { }
 
-  handle(httpRequest: HttpRequest): HttpResponse {
+  handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
@@ -26,10 +25,7 @@ export class SignUpController implements Controller {
         body: 'ok'
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
