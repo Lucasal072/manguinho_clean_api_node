0aad007c20e3384f1207158d9b1518473d37cacd
lucas alves <lucas@pop-os.localdomain>
2024-01-28 01:09:19 -0300

    feat: ensure SignupController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,10 +13,14 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
       return {
         statusCode: 200,
         body: 'ok'
