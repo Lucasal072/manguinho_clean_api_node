c569bc8c024fe269bff3d9500b6404a2b4796401
lucas alves <lucas@pop-os.localdomain>
2024-02-03 19:56:33 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -101,4 +101,20 @@ describe('DbAccount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+  test('Should return an account on sucess', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+      id: 'valid_id'
+    })
+  })
 })
